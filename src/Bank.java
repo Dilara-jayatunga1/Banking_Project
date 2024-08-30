2a32552d43187a2d783488b73a7b433af51ab522
dilara-jayatunga <dilara.jayatunga@aiesec.net>
2024-08-30 09:38:12 +0530

    added a new bank class

---
+++ src/Bank.java
@@ -0,0 +1,21 @@
+  /*
+   * represent a bank for managing customers and their bank accounts
+   */
+
+  public class Bank {
+    public static void main(String[] args) {
+        //create new instance of the bank class
+      Bank bank = new Bank();
+
+      //call the run method in the bank class
+      bank.run();
+
+    }
+    /*
+    run the program by initializing and managing ,bank accounts and customers
+     */
+    public void run(){
+        System.out.println("program running");
+
+    }
+}
